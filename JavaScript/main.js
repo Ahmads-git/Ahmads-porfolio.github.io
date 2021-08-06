8147c84d023192e5ddc7019104c13da6ecf222fc
Ahmad's Git <75440039+Ahmad-develop@users.noreply.github.com>
2021-08-06 12:02:08 +0500

    My Portfolio
    #1

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+
+// let totalWidth = window.innerWidth;
+// let note = document.querySelector('.Note')
+
+
+
+// if(totalWidth > 1330){
+//     note.style.display = 'none'
+// }
+// else if(totalWidth < 1330){
+//     note.style.display = 'block'
+// }
